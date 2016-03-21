5c19d5be646b017e1d1fccaee4bc4d16c155f4a3
Brian Turchyn <brian@dj-bri-t.net>
2016-03-21 07:29:01 -0500

    WIP

--- index.js
+++ index.js
@@ -4,27 +4,14 @@ var LocalStrategy = require('passport-local').Strategy;
 var http = require('http');
 var io = require('socket.io')(3001);
 var util = require('util');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-
-console.log('this actually works');
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    console.log('Attempting local auth');
-    return done(null, { id: 1, user: "foo", password: "bar" });
-  }
-));
+var randomstring = require('randomstring');
+var db = require('./lib/db');
 
 passport.serializeUser(function(user, done) {
-  console.log('Serialize');
-  console.log(user);
   done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
-  console.log('Deserialize');
   done(null, {id: id});
 });
 
@@ -33,18 +20,14 @@ app.use(require('morgan')('combined'));
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(passport.initialize());
 
-/*
- *passport.use(new LocalStrategy(
- *  function(user, password, done) {
- *    console.log('Attempting local strategy');
- *    if(user === "foo" && password === "bar") {
- *      return done(null, { user: "foo", password: "bar" });
- *    } else {
- *      return done(null, false);
- *    }
- *  }
- *));
- */
+
+passport.use(new LocalStrategy(
+  function(user, password, done) {
+    result = db.authenticateUser(user, password, function(result) {
+      return done(null, result);
+    });
+  }
+));
 
 app.post('/api/v1/login', 
     passport.authenticate('local'),
@@ -52,33 +35,10 @@ app.post('/api/v1/login',
       console.log('Passed authentication');
       res.json({
         result: true,
-        token: "abc123"
+        token: randomstring.generate(64)
       });
 });
 
-app.post('/login', passport.authenticate('local'));
-
-/*
- *http.createServer(function(req, res) {
- *  var body = "";
- *  console.log('Request received');
- *  util.log(util.inspect(req));
- *
- *  req.on('data', function(chunk) {
- *    body += chunk;
- *    console.log('GOT DATA');
- *    res.writeHead(200, { 'Content-Type': 'text/plain' });
- *  });
- *
- *  req.on('end', function() {
- *    console.log('POSTed: ' + body);
- *
- *  });
- *
- *  res.end('callback(\'{\"msg\": \"OK\"}\')');
- *}).listen(3000);
- *
- */
 app.listen(3000);
 
 io.on('connection', function(socket) {
@@ -88,6 +48,4 @@ io.on('connection', function(socket) {
     fn('woot');
   });
 });
-
-console.log('this does too');
 
